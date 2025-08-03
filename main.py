aacc172d713542dbad14fc3c0eec42972f74bfe6
js.park <edith006@naver.com>
2025-08-03 14:49:20 +0900

    [Env] fix image name, add batch-analyze
    Signed-off-by: js.park <edith006@naver.com>

--- main.py
+++ main.py
@@ -3,6 +3,19 @@ from detect_utils import analyze_image
 
 app = FastAPI()
 
+@app.post("/batch-analyze/")
+async def batch_analyze(files: list[UploadFile] = File(...)):
+    results = []
+
+    for file in files:
+        image_bytes = await file.read()
+        result = analyze_image(image_bytes)
+        results.append({
+            "filename": file.filename,
+            "result": result
+        })
+
+    return results
 
 @app.post("/analyze/")
 async def analyze(file: UploadFile = File(...)):
